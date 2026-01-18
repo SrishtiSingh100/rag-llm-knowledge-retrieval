93a190fb45916ab1e09a9af6c581ca13fae5499c
SrishtiSingh100 <srishtis1013@gmail.com>
2026-01-18 11:45:12 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name="rag-llm",
+    version="0.1.0",
+    author="Srishti Singh",
+    author_email="srishtis1013@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
